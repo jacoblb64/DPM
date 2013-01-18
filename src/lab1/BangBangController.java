8d6a7f22b5cc4274ab800311393a91dffc84f075
Jacob B <jacoblb64@gmail.com>
2013-01-18 17:19:52 -0500

    Finished lab1

--- src/lab1/BangBangController.java
+++ src/lab1/BangBangController.java
@@ -11,6 +11,7 @@ public class BangBangController implements UltrasonicController{
 	private int distance;
 	private int currentLeftSpeed;
 	private int dCount;
+	private double lastDistance;
 	
 	public BangBangController(int bandCenter, int bandwith, int motorLow, int motorHigh) {
 		//Default Constructor
@@ -30,39 +31,44 @@ public class BangBangController implements UltrasonicController{
 		this.distance = distance;
 		// TODO: process a movement based on the us distance passed in (BANG-BANG style)
 		
-		
-		
+		//variables for calculation
 		double error = this.distance - bandCenter;
+		double delta = Math.abs(this.distance - this.lastDistance);
 		
-		if(distance >= 250 && dCount < 25) {
+		//only accept a maximum distance if it holds for 25 reads, otherwise just go straight
+		if(distance >= 250 && dCount <= 25) {
 			rightMotor.setSpeed(motorStraight);
 			leftMotor.setSpeed(motorStraight);
 			
 			dCount++;
 			return;
 		}
+		if(delta < 5) return;
+		//if the new value is within a distance of 5 from the previous value, just continue current movement
 		
 		
+		//after a maximum distance, once the distance drops below that, reset the counter
 		if(distance < 250) {
 			dCount = 0;
 		}
 		
-		if(Math.abs(error) <= bandwith) {
+		if(Math.abs(error) <= bandwith) { //within the bandwith [sic], go straight
 			
 			rightMotor.setSpeed(motorStraight);
 			leftMotor.setSpeed(motorStraight);
 		}
 
-		else if(error > 0) { //too far
+		else if(error > 0) { //too far, angle left
 			rightMotor.setSpeed(motorHigh);
 			leftMotor.setSpeed(motorLow - 50);
 		}
-		else { //too close
+		else { //too close, angle right
 			rightMotor.setSpeed(motorLow - 50);
 			leftMotor.setSpeed(motorHigh);
-			// added a comment to test Git
 		}
 		
+		this.lastDistance = this.distance;
+		//keeping lastDistance for calculating delta
 	}
 
 	@Override
--- src/lab1/PController.java
+++ src/lab1/PController.java
@@ -5,7 +5,7 @@ import lejos.nxt.*;
 public class PController implements UltrasonicController {
 	
 	private final int bandCenter, bandwith;
-	private final int motorStraight = 200, FILTER_OUT = 20;
+	private final int motorStraight = 350, FILTER_OUT = 20 + 7;
 	private final NXTRegulatedMotor leftMotor = Motor.A, rightMotor = Motor.C;	
 	private int distance;
 	private int currentLeftSpeed;
@@ -40,28 +40,38 @@ public class PController implements UltrasonicController {
 		}
 		// TODO: process a movement based on the us distance passed in (P style)
 		
-		double error = this.distance - bandCenter;
-		int differential = (int) Math.abs( 5*error );
-		differential %= 500;
+		//for calculations
+		double error = this.distance - (bandCenter + 10);
+		int differential = (int) Math.abs( 7*error );
+		if(differential > 400) differential = 400;
+		/* differential will be the amount to change wheel-speeds by
+		 * this is a linear function
+		 * it caps out at 400 to avoid too extreme speeds
+		 */
 		
-		LCD.drawString("Error:" + error, 0, 3);
-		LCD.drawString("Differential: " + differential, 0, 4);
-		LCD.drawString("Rwsp:" + rightMotor.getSpeed(), 0, 5);
-		LCD.drawString("Lwsp:" + leftMotor.getSpeed(), 0, 6);
 		
-		if(Math.abs(error) <= bandwith) {
+		if(Math.abs(error) <= bandwith) { //within bandwith [sic], continue straight
 			rightMotor.setSpeed(motorStraight);
 			leftMotor.setSpeed(motorStraight);
 		}
-		else if(error > 0) { //too far
+		else if(error > 0) { //too far, angle in
+			differential = (int) (differential*0.4);
 			rightMotor.setSpeed(motorStraight + differential);
-			leftMotor.setSpeed(motorStraight - 2*differential);
+			leftMotor.setSpeed(motorStraight - differential);
+			LCD.drawString("FIRST ELSE IF", 0, 7);
 		}
-		else { //too close
-			rightMotor.setSpeed(motorStraight - 2*differential);
+		else { //too close, angle out
+			differential = (int) (differential*2.2);
+			rightMotor.setSpeed(motorStraight - differential);
 			leftMotor.setSpeed(motorStraight + differential);
+			LCD.drawString("SECOND ELSE IF", 0, 7);
 		}
 		
+		
+		LCD.drawString("Error:" + error, 0, 3);
+		LCD.drawString("Diff'l: " + differential, 0, 4);
+		LCD.drawString("Rwsp:" + rightMotor.getSpeed(), 0, 5);
+		LCD.drawString("Lwsp:" + leftMotor.getSpeed(), 0, 6);
 	}
 
 	
