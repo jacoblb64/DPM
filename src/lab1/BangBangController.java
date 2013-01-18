9af1bf114442fc81149cbe6b6b28211df0c87bb5
Jacob B <jacoblb64@gmail.com>
2013-01-18 11:00:05 -0500

    Started project and added lab1

---
+++ src/lab1/BangBangController.java
@@ -0,0 +1,38 @@
+package lab1;
+
+import lejos.nxt.NXTRegulatedMotor;
+import lejos.nxt.*;
+
+public class BangBangController implements UltrasonicController{
+	private final int bandCenter, bandwith;
+	private final int motorLow, motorHigh;
+	private final int motorStraight = 200;
+	private final NXTRegulatedMotor leftMotor = Motor.A, rightMotor = Motor.C;
+	private int distance;
+	private int currentLeftSpeed;
+	
+	public BangBangController(int bandCenter, int bandwith, int motorLow, int motorHigh) {
+		//Default Constructor
+		this.bandCenter = bandCenter;
+		this.bandwith = bandwith;
+		this.motorLow = motorLow;
+		this.motorHigh = motorHigh;
+		leftMotor.setSpeed(motorStraight);
+		rightMotor.setSpeed(motorStraight);
+		leftMotor.forward();
+		rightMotor.forward();
+		currentLeftSpeed = 0;
+	}
+	
+	@Override
+	public void processUSData(int distance) {
+		this.distance = distance;
+		// TODO: process a movement based on the us distance passed in (BANG-BANG style)
+		
+	}
+
+	@Override
+	public int readUSDistance() {
+		return this.distance;
+	}
+}
