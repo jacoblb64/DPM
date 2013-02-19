1c39a0fe2579d7ea413bd31399ccc7a7a2601b68
unknown <jbarne13@156TR0110DM-07.campus.MCGILL.CA>
2013-02-18 20:54:35 -0500

    lab5
    Signed-off-by: unknown <jbarne13@156TR0110DM-07.campus.MCGILL.CA>

--- src/lab5/Lab5.java
+++ src/lab5/Lab5.java
@@ -3,6 +3,7 @@ package lab5;
 import lejos.nxt.Button;
 import lejos.nxt.Motor;
 import lejos.nxt.NXTRegulatedMotor;
+import bluetooth.BluetoothConnection;
 
 public class Lab5 {
 
@@ -14,36 +15,52 @@ public class Lab5 {
 				System.exit(0);
 			}
 		}).start();
-	
 		
-		
-		if (Button.waitForAnyPress() == Button.ID_LEFT) {
+		try {
+			Exception la = new Exception();
+			throw la;
+			/*
+			BluetoothConnection btooth = new BluetoothConnection();
+			btooth.getTransmission();
+			btooth.printTransmission();
 			
-			for (NXTRegulatedMotor motor : new NXTRegulatedMotor[] { Motor.A, Motor.C }) {
-				motor.setAcceleration(400);
-				motor.setSpeed(motor.getMaxSpeed());
+			catapult();
+			*/
+		}
+		catch (Exception ee){		
+			if (Button.waitForAnyPress() == Button.ID_LEFT) {
+				flywheel();
 			}
 			
-			try { Thread.sleep(2000); } catch (InterruptedException e) { e.printStackTrace(); }
-			
-			for (NXTRegulatedMotor motor2 : new NXTRegulatedMotor[] { Motor.A, Motor.C }) {
-				motor2.forward();
+			if (Button.waitForAnyPress() == Button.ID_RIGHT) {			
+				catapult();
 			}
 		}
-		
-		if (Button.waitForAnyPress() == Button.ID_RIGHT) {
-			
-			for (NXTRegulatedMotor motor : new NXTRegulatedMotor[] { Motor.A, Motor.C }) {
-				motor.setAcceleration(10000);
-				motor.setSpeed(motor.getMaxSpeed());
-			}
-			
-			try { Thread.sleep(500); } catch (InterruptedException e) { e.printStackTrace(); }
-			
-			for (NXTRegulatedMotor motor : new NXTRegulatedMotor[] { Motor.A, Motor.C }) {
-				motor.rotate(30, true);
-			}
-		}
+	}
 	
+	private static void flywheel() {
+		for (NXTRegulatedMotor motor : new NXTRegulatedMotor[] { Motor.A, Motor.B, Motor.C   }) {
+			motor.setAcceleration(400);
+			motor.setSpeed(motor.getMaxSpeed());
+		}
+		
+		try { Thread.sleep(2000); } catch (InterruptedException e) { e.printStackTrace(); }
+		
+		for (NXTRegulatedMotor motor2 : new NXTRegulatedMotor[]  {Motor.A, Motor.B, Motor.C }) {
+			motor2.forward();
+		}
+	}
+	
+	private static void catapult() {
+		for (NXTRegulatedMotor motor : new NXTRegulatedMotor[] { Motor.A, Motor.B, Motor.C }) {
+			motor.setAcceleration(10000);
+			motor.setSpeed(motor.getMaxSpeed());
+		}
+		
+		try { Thread.sleep(500); } catch (InterruptedException e) { e.printStackTrace(); }
+		
+		for (NXTRegulatedMotor motor : new NXTRegulatedMotor[] { Motor.A, Motor.B, Motor.C }) {
+			motor.rotate(110 , true);
+		}
 	}
 }
